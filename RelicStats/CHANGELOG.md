e2c93aeaae36018beb86346963dc7b7d64bb4558
phlip <git@mrphlip.com>
2024-10-02 15:00:25 +1000

    Fix some places that called it RelicColour, make RelicStats skeleton

---
+++ RelicStats/CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.2.0
+* Initial public release
+* Re-colours buttons in: post-battle upgrades, most scenarios that offer you relics, shops, and for some reason the "what's currently in my deck" info popup.
---
+++ RelicStats/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RelicStats;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Peglin.exe")]
+[HarmonyPatch]
+public class Plugin : BaseUnityPlugin {
+	private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+	internal static new ManualLogSource Logger;
+
+	private void Awake() {
+		harmony.PatchAll();
+		Logger = base.Logger;
+		Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+	}
+}
